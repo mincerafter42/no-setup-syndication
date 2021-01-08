f0f7389efcb641f94f69ac659c576ffcccbbbd84
mincerafter42 <mincerafter42@gmail.com>
2021-01-07 18:36:22 -0800

    Made extension slightly more visually appealing.

--- popup.js
+++ popup.js
@@ -18,8 +18,8 @@ document.getElementById("feedAddConfirm").addEventListener("click", function() {
 	document.getElementById("feedAdder").style.display="none"; // hide feedAdder
 });
 
-chrome.runtime.sendMessage({message: "getFeedContents"}, {}, ({response})=>{ // send the message {message: "getFeedContents"}
-	/* function parsing response to that message, should be the result of a Promise.allSettled()
+chrome.runtime.sendMessage({message: "getFeedContents"}, {}, ({response, lastViewed})=>{ // send the message {message: "getFeedContents"}
+	/* function parsing {response} to that message, should be the result of a Promise.allSettled()
 	need to read feeds from promises, put items in chronological order, and display items */
 	let combinedItems=[]; // combinedItems will fill with items from all feeds
 	for (let feed=0;feed<response.length;feed++) { // iterate through every Promise in the array
@@ -27,11 +27,18 @@ chrome.runtime.sendMessage({message: "getFeedContents"}, {}, ({response})=>{ // 
 			combinedItems.push(...response[feed].value.item); // push items from this feed into combinedItems
 		}
 	}
-	console.log(combinedItems)
 	combinedItems.sort((item1, item2)=>item2.pubDate-item1.pubDate); // sort items by pubDate, more recent items first
 	// now we just need to display the items in combinedItems
+	const dateFormat = new Intl.DateTimeFormat();
 	for (let item=0;item<combinedItems.length;item++) { // iterate through every item in combinedItems
 		let itemDisplay = document.createElement("div"); // make a div
+		if (combinedItems[item].pubDate>lastViewed) itemDisplay.className = "item unread"; // if this item is more recent than last read, give it the unread class
+		else itemDisplay.className = "item read"; // if it's less recent than last read give it the read class
+		
+		let itemMeta = document.createElement("div");
+		itemMeta.className = "meta";
+		itemDisplay.appendChild(itemMeta)
+		
 		let itemTitle = document.createElement("h2"); // make an h2 with the item's title and put it in the div
 		if (combinedItems[item].link) {
 			let itemLink = document.createElement("a");
@@ -41,8 +48,15 @@ chrome.runtime.sendMessage({message: "getFeedContents"}, {}, ({response})=>{ // 
 			itemTitle.appendChild(itemLink);
 		}
 		else itemTitle.textContent = combinedItems[item].title;
-		itemDisplay.appendChild(itemTitle);
+		itemMeta.appendChild(itemTitle);
+		
+		let itemTime = document.createElement("span");
+		itemTime.className = "time";
+		itemTime.textContent = dateFormat.format(new Date(combinedItems[item].pubDate));
+		itemMeta.appendChild(itemTime);
+		
 		let itemDescription = document.createElement("div"); // make a div with the item's description and put it in the div
+		itemDescription.className = "description"
 		itemDescription.innerHTML = combinedItems[item].description;
 		let itemDescriptionLinks = itemDescription.getElementsByTagName("a"); // getting all the links in the description to make sure they open in new tabs and are absolute URLs
 		for (const link of itemDescriptionLinks) { // iterate through links (any order)
